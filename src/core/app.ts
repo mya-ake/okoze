cb67cc1c3d83bf16d497805a6591065dd3724303
mya-ake <make.app.ts@gmail.com>
2019-04-09 17:22:29 +0900

    refactor: app

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,98 +1,20 @@
 import express = require('express');
 import bodyParser = require('body-parser');
-import axios, { AxiosRequestConfig } from 'axios';
-import { join } from 'path';
 
-import { wrightFile, existPathname, readFile } from './../lib/file';
+import { buildProcessor } from './processor';
 import { buildLoggerMiddleware } from './../middlewares/loggerMiddleWare';
-import { OkozeResponse, OkozeOptions } from 'src/types';
-
-const createSnapshotPathname = ({
-  method,
-  url,
-}: {
-  method: string;
-  url: string;
-}) => {
-  return `${method}:${encodeURIComponent(url)}.json`;
-};
+import { OkozeOptions } from 'src/types';
 
 const buildApp = (options: OkozeOptions) => {
   const app = express();
-  const { origin: baseURL, snapshotDir } = options;
-
-  const request = axios.create({
-    baseURL,
-    adapter: require('axios/lib/adapters/http'),
-  });
-
-  const requestOrigin = async (
-    { snapshotPathname }: { snapshotPathname: string },
-    {
-      method,
-      url,
-      headers,
-      body,
-    }: { method: string; url: string; headers: any; body: any },
-  ): Promise<OkozeResponse> => {
-    const axiosConfig: AxiosRequestConfig = {
-      method,
-      url,
-      data: body,
-      headers: {
-        ...headers,
-      },
-    };
-
-    const { status, data, headers: responseHeaders } = await request(
-      axiosConfig,
-    ).catch(err => err.response || {});
-
-    await wrightFile(snapshotPathname, {
-      status,
-      data,
-      headers: responseHeaders,
-    });
-
-    return {
-      status,
-      data,
-      headers: responseHeaders,
-    };
-  };
-
-  const readSnapshot = async ({
-    snapshotPathname,
-  }: {
-    snapshotPathname: string;
-  }): Promise<OkozeResponse> => {
-    const { status, data, headers } = await readFile(snapshotPathname);
-    return { status, data, headers };
-  };
+  const { origin: baseURL } = options;
 
   app.use(bodyParser.json());
   if (process.env.DEBUG === 'true') {
     app.use(buildLoggerMiddleware({ baseURL }));
   }
 
-  app.all('*', async (req: express.Request, res: express.Response) => {
-    const snapshotFilename = createSnapshotPathname(req);
-    const snapshotPathname = join(snapshotDir, snapshotFilename);
-
-    const existSnapshot = await existPathname(snapshotPathname);
-
-    const { status, data, headers } = existSnapshot
-      ? await readSnapshot({ snapshotPathname })
-      : await requestOrigin({ snapshotPathname }, req);
-
-    res.status(status);
-    res.set({
-      ...headers,
-      'cache-control': 'no-store',
-      'x-okoze-snapshot': existSnapshot ? 'used' : 'update',
-    });
-    res.json(data);
-  });
+  app.all('*', buildProcessor(options));
 
   return app;
 };
---
+++ src/core/processor.ts
@@ -0,0 +1,90 @@
+import express = require('express');
+import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
+import { join } from 'path';
+import { wrightFile, existPathname, readFile } from './../lib/file';
+import { OkozeOptions, OkozeResponse } from 'src/types';
+
+const createSnapshotPathname = ({
+  method,
+  url,
+}: {
+  method: string;
+  url: string;
+}) => {
+  return `${method}:${encodeURIComponent(url)}.json`;
+};
+
+const readSnapshot = async ({
+  snapshotPathname,
+}: {
+  snapshotPathname: string;
+}): Promise<OkozeResponse> => {
+  const { status, data, headers } = await readFile(snapshotPathname);
+  return { status, data, headers };
+};
+
+const buildRequestOrigin = (request: AxiosInstance) => {
+  return async (
+    { snapshotPathname }: { snapshotPathname: string },
+    {
+      method,
+      url,
+      headers,
+      body,
+    }: { method: string; url: string; headers: any; body: any },
+  ): Promise<OkozeResponse> => {
+    const axiosConfig: AxiosRequestConfig = {
+      method,
+      url,
+      data: body,
+      headers: {
+        ...headers,
+      },
+    };
+
+    const { status, data, headers: responseHeaders } = await request(
+      axiosConfig,
+    ).catch(err => err.response || {});
+
+    await wrightFile(snapshotPathname, {
+      status,
+      data,
+      headers: responseHeaders,
+    });
+
+    return {
+      status,
+      data,
+      headers: responseHeaders,
+    };
+  };
+};
+
+export const buildProcessor = (options: OkozeOptions) => {
+  const { origin: baseURL, snapshotDir } = options;
+  const request = axios.create({
+    baseURL,
+    adapter: require('axios/lib/adapters/http'),
+  });
+
+  const requestOrigin = buildRequestOrigin(request);
+
+  return async (req: express.Request, res: express.Response) => {
+    const snapshotFilename = createSnapshotPathname(req);
+    const snapshotPathname = join(snapshotDir, snapshotFilename);
+
+    const existSnapshot = await existPathname(snapshotPathname);
+
+    const { status, data, headers } = existSnapshot
+      ? await readSnapshot({ snapshotPathname })
+      : await requestOrigin({ snapshotPathname }, req);
+
+    res.status(status);
+    res.set({
+      ...headers,
+      'cache-control': 'no-store',
+      'x-okoze-snapshot': existSnapshot ? 'used' : 'update',
+    });
+    res.json(data);
+  };
+};
